ca695b1a808a77a73c1164fb799ec59fc5d2b6f1
Quentin McMillian <quentinmcmillian@gmail.com>
2020-11-20 11:35:57 -0500

    Added a few additional server unit test

--- server/index.spec.js
+++ server/index.spec.js
@@ -1,8 +1,24 @@
-// const app = require('./index.js');
-// const request = require('supertest');
+const app = require('./index.js');
+const request = require('supertest');
 
-// test('should return 200 status code', async (done)=> {
-//     const res = await request(app).get('/images');
-//     expect(res.status).toBe(200);
-//     done();
-// })
+test('should return 200 status code', async (done)=> {
+    const res = await request(app).get('/images');
+    expect(res.status).toBe(200);
+    done();
+})
+
+
+test('should return a response object that items used array', async (done)=> {
+  const res = await request(app).get('/images');
+  const itemsProp = res.body.Items_used
+  expect(Array.isArray(itemsProp)).toEqual(true);
+  done();
+})
+
+
+test('should return a response that propertiess match schema', async (done) => {
+  const res = await request(app).get('/member');
+  const schema = ["_id", "Name", "Skin_type", "PrimaryId", "__v"]
+  expect(Object.keys(res.body)).toEqual(schema)
+  done();
+})
