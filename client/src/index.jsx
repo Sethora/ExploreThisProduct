3749fd45eccce6ac1e475509de9df865e1a6e3b2
Quentin McMillian <quentinmcmillian@gmail.com>
2021-02-02 23:30:32 -0500

    Removed App component from index.jsx and created App.jsx file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,48 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-import ExploreThisProduct from './components/ETPModule.jsx';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      memberImages: [],
-      hasData: false
-    };
-    this.setImages = this.setImages.bind(this);
-  }
-
-  setImages(array) {
-    this.setState({
-      memberImages: array,
-      hasData: true
-    });
-  }
-
-  componentDidMount() {
-    const url = window.location.href.split('/');
-    const candidateId = url[url.length - 1];
-    const id = (isNaN(Number(candidateId)) || candidateId === '') ? 2 : Number(candidateId);
-    axios.get(`/api/items/images/${id}`)
-      .then((response)=> {
-        this.setImages(response.data);
-      })
-      .catch((error)=>{
-        console.log(error);
-      });
-  }
-
-
-  render() {
-    return (
-      <div>
-        {this.state.hasData ?
-          <ExploreThisProduct images={this.state.memberImages}/> : null
-        }
-      </div>
-    );
-  }
-}
+import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('etpApp'));
