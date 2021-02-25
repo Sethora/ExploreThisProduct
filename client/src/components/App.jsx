67a3050abac78aa7da38cdb315304dc62b65ca25
Quentin McMillian <quentinmcmillian@gmail.com>
2021-02-24 22:08:26 -0500

    Switched App over to React Hooks

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,47 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import ExploreThisProduct from './ETPModule.jsx';
 import {getImages} from '../../api_helpers/helpers.js';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      memberImages: [],
-      hasData: false
-    };
-    this.setImages = this.setImages.bind(this);
-  }
 
-  setImages(array) {
-    this.setState({
-      memberImages: array,
-      hasData: true
-    });
-  }
+const App = () => {
+  const [memberImages, setMemberImages] = useState([]);
+  const [hasData, setHasData] = useState(true);
 
-  componentDidMount() {
+  useEffect(() => {
     const url = window.location.href.split('/');
     const candidateId = url[url.length - 1];
     const id = (isNaN(Number(candidateId)) || candidateId === '') ? 2 : Number(candidateId);
     getImages(id)
       .then(imageData => {
-        this.setImages(imageData);
+        setMemberImages(imageData);
       })
       .catch((error)=>{
         console.log(error);
       });
-  }
+  });
 
-  render() {
-    return (
-      <div>
-        {this.state.hasData ?
-          <ExploreThisProduct images={this.state.memberImages}/> : null
-        }
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {hasData ?
+        <ExploreThisProduct images={memberImages}/> : null
+      }
+    </div>
+  );
+};
 
 export default App;
